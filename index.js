d8c397982cc419e2f3daf9e3b7ce2f5f31fc8d15
Ben Holland <benholland99@gmail.com>
2016-11-11 01:23:45 +0000

    More code

--- index.js
+++ index.js
@@ -1,14 +1,66 @@
-var exec = require('exec-ssh');
-var wol = require('wol');
-var args = require('yargs').argv;
+var series = require('es6-promise-series');
+var fs = require('fs');
+var glob = require('glob');
+var request = require('request');
+var exec = require('ssh-exec');
+var wol = require('node-wol');
 
-wol.wake('', error => {
-    if (error) {
-        throw Error(error);
-    }
+// var PROCESSING_FOLDER = '//NAS/To Be Sorted';
+var CONVERTER_FILE = 'C:/mp4_converter/manual.py';
+var SSH_CREDENTIALS = 'admin@192.168.0.18';
+var PC_MAC_ADDRESS = '50-E5-49-EF-98-39';
+var PROCESSING_FOLDER = 'C:/Users/ben/Desktop/Test';
+var DEST_FOLDER = 'C:/Users/ben/Desktop/Dest';
+var WEBHOOK_URL = 'https://hooks.slack.com/services/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';
 
-    var command = ['node', args.script].join(' ');
+getFilesToProcess()
+    .then(files => series(files.map(processFile)))
+    .then(console.log)
+    .catch(console.error);
 
-    exec(command, args.user).pipe(process.stdout);
-    exec('shutdown -s', args.user).pipe(process.stdout);
-});
+function getFilesToProcess() {
+    return new Promise((resolve, reject) => {
+        glob(`${PROCESSING_FOLDER}/**/*`, (error, files) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(files);
+            }
+        })
+    });
+}
+
+function processFile(file) {
+    var command = `python ${CONVERTER_FILE} -a -i ${file}`;
+
+    return new Promise((resolve, reject) => {
+        console.info('Waking up PC');
+
+        // wol.wake(PC_MAC_ADDRESS, function(error) {
+        //     if (error) {
+        //         reject(error);
+        //     } else {
+        console.info(`Attempting to transcode ${file}`);
+        console.log(`python ${CONVERTER_FILE} -a -i '${file}'`);
+
+        setTimeout(() => {
+            resolve(file)
+        }, 5000);
+
+        exec(command, SSH_CREDENTIALS);
+        // }
+        // });
+    });
+}
+
+function sendNotification(text) {
+    request.post(WEBHOOK_URL, {
+        json: {
+            text
+        }
+    }, (error, response, body) => {
+        if (error) {
+            throw Error(error);
+        }
+    });
+}
