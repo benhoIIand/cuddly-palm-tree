92e67dd24811379751ec08de63ded6b5d32d1619
Ben Holland <benholland99@gmail.com>
2017-01-02 22:36:50 +0000

    Correct convert command

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ var request = require('request');
 var exec = require('ssh-exec');
 var wol = require('node-wol');
 
-// var PROCESSING_FOLDER = '//NAS/To Be Sorted';
 var CONVERTER_FILE = 'C:/mp4_converter/manual.py';
-var SSH_CREDENTIALS = 'admin@192.168.0.18';
+var SSH_CREDENTIALS = 'admin@nas';
 var PC_MAC_ADDRESS = '50-E5-49-EF-98-39';
-var PROCESSING_FOLDER = 'C:/Users/ben/Desktop/Test';
-var DEST_FOLDER = 'C:/Users/ben/Desktop/Dest';
+var PROCESSING_FOLDER = 'C:/Users/ben/Projects/cuddly-palm-tree/temp/ready';
+var DEST_FOLDER = 'C:/Users/ben/Projects/cuddly-palm-tree/temp/done';
 var WEBHOOK_URL = 'https://hooks.slack.com/services/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';
 
 getFilesToProcess()
@@ -31,25 +30,27 @@ function getFilesToProcess() {
 }
 
 function processFile(file) {
-    var command = `python ${CONVERTER_FILE} -a -i ${file}`;
+    var command = `python ${CONVERTER_FILE} -a -i "${file}" -m "${DEST_FOLDER}"`;
 
     return new Promise((resolve, reject) => {
         console.info('Waking up PC');
 
-        // wol.wake(PC_MAC_ADDRESS, function(error) {
-        //     if (error) {
-        //         reject(error);
-        //     } else {
-        console.info(`Attempting to transcode ${file}`);
-        console.log(`python ${CONVERTER_FILE} -a -i '${file}'`);
+        wol.wake(PC_MAC_ADDRESS, function(error) {
+            if (error) {
+                reject(error);
+            } else {
+                console.info(`Attempting to transcode ${file}`);
+                console.info('Command:', command);
 
-        setTimeout(() => {
-            resolve(file)
-        }, 5000);
+                setTimeout(() => {
+                    resolve(file)
+                }, 5000);
 
-        exec(command, SSH_CREDENTIALS);
-        // }
-        // });
+                exec(command, SSH_CREDENTIALS, function() {
+                    resolve(file);
+                });
+            }
+        });
     });
 }
 
